21d01d870ed7f460c241ec90b726c3680136fe26
DBEngRoty <101518092+DBEngRoty@users.noreply.github.com>
2025-11-30 21:45:39 -0500

    Optimize user access script for SQLHKTUsr
    Refactor SQL script to streamline user creation and role assignment.

--- Security/UserAccesAllDBs.sql
+++ Security/UserAccesAllDBs.sql
@@ -1,67 +1,28 @@
 CREATE LOGIN [SQLHKTUsr] WITH PASSWORD = 'SkyIsBlue4!Today';
 
  
-
- 
-
 DECLARE @DBName SYSNAME;
-
 DECLARE @SQL NVARCHAR(MAX);
-
- 
-
 DECLARE db_cursor CURSOR FOR
-
-SELECT name
-
-FROM sys.databases
-
-WHERE database_id > 4  -- Exclude system DBs
-
-  AND state_desc = 'ONLINE';
-
- 
+SELECT name FROM sys.databases WHERE database_id > 4  -- Exclude system DBs AND state_desc = 'ONLINE';
 
 OPEN db_cursor;
-
 FETCH NEXT FROM db_cursor INTO @DBName;
-
- 
-
 WHILE @@FETCH_STATUS = 0
-
 BEGIN
-
     SET @SQL = '
-
     USE [' + @DBName + '];
-
     IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = ''SQLHKTUsr'')
-
     BEGIN
-
         CREATE USER [SQLHKTUsr] FOR LOGIN [SQLHKTUsr];
-
     END;
-
     EXEC sp_addrolemember ''db_owner'', ''SQLHKTUsr'';';
-
- 
-
     PRINT @SQL;  -- For debugging
-
     EXEC sp_executesql @SQL;
-
- 
-
     FETCH NEXT FROM db_cursor INTO @DBName;
-
 END;
 
- 
-
 CLOSE db_cursor;
-
 DEALLOCATE db_cursor;
 
  
